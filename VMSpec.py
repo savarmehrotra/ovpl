d21a2e654b1e68375180f74c37e3df6dce6975e4
avinassh <avinashsajjan@gmail.com>
2013-12-06 12:12:20 +0530

    modified VMSpec.py, now vm will be created even if user does not provide specs

--- VMSpec.py
+++ VMSpec.py
@@ -8,8 +8,8 @@ class VMSpec:
     def __init__(self, specs):
         # Temporarily setting defaults
         self.lab_ID = specs['lab_ID'] #"test99"
-        self.os = specs['os'] #"Ubuntu"
-        self.os_version = specs['os_version'] #"12.04"
-        self.ram = specs['ram'] #"256M"
-        self.swap = specs['swap'] #"512M"
-        self.diskspace = specs['diskspace'] #"2G"
+        self.os = specs.get('os', 'Ubuntu') #"Ubuntu"
+        self.os_version = specs.get('os_version', '12.04') #"12.04"
+        self.ram = specs.get('ram', '256M') #"256M"
+        self.swap = specs.get('swap', '512M') #"512M"
+        self.diskspace = specs.get('diskspace', '2G') #"2G"
