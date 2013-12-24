f37719e375fd0af9a7466e2ba356869226cde3dc
avinassh <avinashsajjan@gmail.com>
2013-12-24 12:06:21 +0530

    added first version of VM Manager Server

--- VMManagerServer.py
+++ VMManagerServer.py
@@ -14,10 +14,9 @@ import tornado.httpserver
 import tornado.ioloop 
 import tornado.options 
 import tornado.web
+from tornado.options import define, options
 
 import VMManager
-
-from tornado.options import define, options
 
 
 define("port", default=8000, help="run on the given port", type=int)
