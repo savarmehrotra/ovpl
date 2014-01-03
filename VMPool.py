2088f247aaac1176fd4b8644aca8376817479dcb
avinassh <avinashsajjan@gmail.com>
2014-01-03 10:34:48 +0530

    added instructions about Requests module

--- VMPool.py
+++ VMPool.py
@@ -1,7 +1,39 @@
 # Author: Chandan Gupta
 # Contact: chandan@vlabs.ac.in
 
-""" VMPool module description """
+""" VMPool module description 
+
+Requests, a 3rd-party Python module, is used to make requests to VM Pool and 
+the response which is sent back to the caller is Requests' response object. 
+
+The response object supports following:
+- response_object.status_code
+- response_object.headers
+- response_object.url
+- response_object.text, provides the response body
+- response_object.content, provides binary response content 
+- response_object.json(), provides response content in JSON 
+
+The Requests module supports GET, POST, PUT, DELETE and OPTIONS requests. In
+case of POST or PUT request, the request data should be in Python dictionary. 
+The requests module will handle converting it to proper POST/PUT url format
+
+Example usage: 
+
+>>>import requests
+>>>response_object = requests.get(url='http://www.google.com')
+>>>print response_object.status_code
+
+>>>payload = {'username': 'avinash', 'password': 'avinash'}
+>>>response_object_post = requests.post(url='http://www.google.com/login', 
+                                        data=payload)
+
+>>>response_object_put = requests.put(url='http://www.google.com/login', 
+                                        data=payload)
+
+>>>response_object_delete = requests.delete(url='http://google.com/accounts')
+>>>print response_object_delete.text
+"""
 
 __all__ = [
     'VMProxy',
