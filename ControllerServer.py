cc3ee184af7245407024a38fdaab43338b7c036f
avinassh <avinashsajjan@gmail.com>
2014-01-31 18:16:09 +0530

    add Controller Server and supporting template files

---
+++ ControllerServer.py
@@ -0,0 +1,49 @@
+#!/bin/python
+
+# Services exposed by the VM Manager
+# The REST url : 
+# http://host-name/api/1.0/disk-usage
+# http://host-name/api/1.0/running-time 
+# http://host-name/api/1.0/mem-usage
+# http://host-name/api/1.0/running-processes
+# http://host-name/api/1.0/cpu-load
+# http://host-name/api/1.0/execute/<command>
+
+import urlparse
+import os
+
+# bunch of tornado imports
+import tornado.httpserver 
+import tornado.ioloop 
+import tornado.options 
+import tornado.web
+from tornado.options import define, options
+
+import Controller
+
+
+define("port", default=8000, help="run on the given port", type=int)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+    def post(self):
+        post_data = dict(urlparse.parse_qsl(self.request.body))
+        c = Controller.Controller()
+        self.write(c.test_lab(post_data['lab_id'], post_data['lab_src_url'], post_data.get('version', None)))
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers=[
+            (r"/", MainHandler)
+        ],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        debug = True)
+    http_server = tornado.httpserver.HTTPServer(app) 
+    http_server.listen(options.port) 
+    tornado.ioloop.IOLoop.instance().start()
