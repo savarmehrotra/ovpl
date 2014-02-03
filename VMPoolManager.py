0f71daf5d7e5ba3e35478d0cb86c9dc9a29b7276
Chandan Kumar <chandan.gupta@gmail.com>
2014-02-03 09:13:54 +0530

    Added logging and some placeholder functions

--- VMPoolManager.py
+++ VMPoolManager.py
@@ -5,20 +5,38 @@ and decide which VMPool to use for creating a VM.
 """
 
 import VMPool
+from OVPLLogging import *
 
 class VMPoolManager:
     def __init__(self):
         self.VMPools = []
+        self.add_vm_pool("http://localhost", "8000")        # Adapter IP and Port
 
     def add_vm_pool(self, adapter_ip, adapter_port):
         self.VMPools.append(VMPool.VMPool(adapter_ip, adapter_port))
 
     def get_available_pool(self, lab_spec):
-        return self.VMPools[0]
+        """Imagining four VMPools:
+        0. For Linux VMs in private data center (uses OpenVZ)
+        1. For Amazon S3 (static content hosting)
+        2. For Windows VMs in private data center (uses KVM)
+        3. For Amazon EC2
+        
+        """
+        if self.is_lab_static(lab_spec):
+            return self.VMPools[1]
+        elif self.lab_on_windows(lab_spec):
+            return self.VMPools[2]
+        else:
+            return self.VMPools[0]
+
+    def is_lab_static(self, lab_spec):
+        return False
+
+    def lab_on_windows(self, lab_spec):
+        return False
 
     def create_vm(self, lab_spec):
-        print "VMPoolManager.create_vm()"
-        #dirty hack
-        self.add_vm_pool("http://localhost", "8000")        # Adapter IP and Port
+        OVPL_LOGGER.debug("VMPoolManager.create_vm()")
         vmpool = self.get_available_pool(lab_spec)
         return vmpool.create_vm(lab_spec)
