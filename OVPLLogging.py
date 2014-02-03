c9acbe45d257d971bf55fc2a44365a0f7f37c466
Chandan Kumar <chandan.gupta@gmail.com>
2014-02-03 16:22:09 +0530

    Wrapped initialization code in a class

--- OVPLLogging.py
+++ OVPLLogging.py
@@ -1,21 +1,29 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
-LOG_FILENAME = '/root/ovpl/log/ovpl.log'       # make log name a setting
 
-def setup_logging():
-    OVPL_LOGGER.setLevel(logging.DEBUG)   # make log level a setting
-    # Add the log message handler to the logger
-    myhandler = TimedRotatingFileHandler(
-                                LOG_FILENAME, when='midnight', backupCount=5)
+class OVPLLogging:
 
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %I:%M:%S %p')
-    myhandler.setFormatter(formatter)
-    OVPL_LOGGER.addHandler(myhandler)
+    @staticmethod
+    def setup_logging():
+        OVPL_LOGGER.setLevel(logging.DEBUG)   # make log level a setting
+        # Add the log message handler to the logger
+        myhandler = TimedRotatingFileHandler(
+                                    LOG_FILENAME, when='midnight', backupCount=5)
+
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            datefmt='%Y-%m-%d %I:%M:%S %p')
+        myhandler.setFormatter(formatter)
+        OVPL_LOGGER.addHandler(myhandler)
 
 
-OVPL_LOGGER = logging.getLogger('ovpl')
-setup_logging()
-LOG_FD = open(LOG_FILENAME, 'a')
+try:
+    OVPLLogging.OVPL_LOGGER
+except Exception, e:
+    LOG_FILENAME = '/root/ovpl/log/ovpl.log'       # make log name a setting
+    print "Initializing OVPL_LOGGER and LOG_FD"
+    OVPL_LOGGER = logging.getLogger('ovpl')
+    OVPLLogging.setup_logging()
+    LOG_FD = open(LOG_FILENAME, 'a')
+
