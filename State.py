80c78ba5f3f2ec1ce84bb266958e58fdaa17b05d
Chandan Kumar <chandan.gupta@gmail.com>
2014-02-05 07:56:54 +0530

    Initial version of save and restore state using mongodb

---
+++ State.py
@@ -0,0 +1,21 @@
+import pymongo
+from Singleton import Singleton
+
+@Singleton
+class State:
+    def __init__(self):
+        self.db = pymongo.MongoClient().ovpl
+        self.restore()
+
+    def restore(self):
+        """Restores state from a mongodb collection."""
+        if "ovpl" in self.db.collection_names():
+            self.state = list(self.db.ovpl.find())
+        else:
+            self.state = []
+
+    def save(self):
+        """Writes the current state to mongodb(disk)"""
+        if "ovpl" in self.db.collection_names():
+            self.db.ovpl.rename("ovpl-last", dropTarget=True)
+        self.db.ovpl.insert(self.state)
