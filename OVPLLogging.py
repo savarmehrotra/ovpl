a9a2388989d4f962d323867bd219440bbac76801
Chandan Kumar <chandan.gupta@gmail.com>
2014-02-05 06:48:46 +0530

    This wrapping in class is not required as Python's import ensures that the module is only run once.
    Revert "Wrapped initialization code in a class"
    
    This reverts commit c9acbe45d257d971bf55fc2a44365a0f7f37c466.

--- OVPLLogging.py
+++ OVPLLogging.py
@@ -1,29 +1,21 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
+LOG_FILENAME = '/root/ovpl/log/ovpl.log'       # make log name a setting
 
-class OVPLLogging:
+def setup_logging():
+    OVPL_LOGGER.setLevel(logging.DEBUG)   # make log level a setting
+    # Add the log message handler to the logger
+    myhandler = TimedRotatingFileHandler(
+                                LOG_FILENAME, when='midnight', backupCount=5)
 
-    @staticmethod
-    def setup_logging():
-        OVPL_LOGGER.setLevel(logging.DEBUG)   # make log level a setting
-        # Add the log message handler to the logger
-        myhandler = TimedRotatingFileHandler(
-                                    LOG_FILENAME, when='midnight', backupCount=5)
-
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            datefmt='%Y-%m-%d %I:%M:%S %p')
-        myhandler.setFormatter(formatter)
-        OVPL_LOGGER.addHandler(myhandler)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %I:%M:%S %p')
+    myhandler.setFormatter(formatter)
+    OVPL_LOGGER.addHandler(myhandler)
 
 
-try:
-    OVPLLogging.OVPL_LOGGER
-except Exception, e:
-    LOG_FILENAME = '/root/ovpl/log/ovpl.log'       # make log name a setting
-    print "Initializing OVPL_LOGGER and LOG_FD"
-    OVPL_LOGGER = logging.getLogger('ovpl')
-    OVPLLogging.setup_logging()
-    LOG_FD = open(LOG_FILENAME, 'a')
-
+OVPL_LOGGER = logging.getLogger('ovpl')
+setup_logging()
+LOG_FD = open(LOG_FILENAME, 'a')
