8fee37c7568b6540100a03cffe35cf32f995dd5a
Chandan Kumar <chandan.gupta@gmail.com>
2014-02-06 21:44:39 +0530

    Moved persistence code to a separate State module.
    Also started implementing undeploy_lab use case.

--- Controller.py
+++ Controller.py
@@ -4,67 +4,63 @@ Controller interfaces with LabManager and VMPoolManager.
 
 """
 
-import pymongo
+#from time import strftime
+from datetime import datetime
 
 import LabManager
 import VMPoolManager
 import Logging
+from State import State
 
 
 class Controller:
     def __init__(self):
-        self.db = pymongo.MongoClient().ovpl
-        self.restore_state()
+        self.system = State.Instance()
 
-    def restore_state(self):
-        """Restores state from a mongodb collection."""
-        if "ovpl" in self.db.collection_names():
-            self.state = list(self.db.ovpl.find())
-        else:
-            self.state = []
-
-    def save_state(self):
-        #Writes the current state to mongodb(disk)
-        if "ovpl" in self.db.collection_names():
-            self.db.ovpl.rename("ovpl-last", dropTarget=True)
-        self.db.ovpl.insert(self.state)
-
-    def test_lab(self, lab_id, lab_src_url, version=None):
+    def test_lab(self, lab_id, lab_src_url, revision_tag=None):
         Logging.LOGGER.debug("Controller.test_lab() for lab ID %s and git url %s" \
                             % (lab_id, lab_src_url))
-        def update_state():
-            self.state.append({ "lab_id": lab_id,
-                                "lab_src_url": lab_src_url,
-                                "revision_tag": version,
-                                "lab_spec": lab_spec,
-                                "vm_ip": ip,
-                                "vmm_url": vmmgrurl,
-                                "vmm_port": port
-                                })
         try:
-            lab_spec = LabManager.get_lab_reqs(lab_id, lab_src_url, version)
+            lab_spec = LabManager.get_lab_reqs(lab_id, lab_src_url, revision_tag)
             lab_spec['lab_id'] = lab_id
             lab_spec['lab_src_url'] = lab_src_url
+            lab_spec['revision_tag'] = revision_tag
+            lab_spec['lab']['runtime_requirements']['hosting'] = 'dedicated'
             vmpoolmgr = VMPoolManager.VMPoolManager()
-            (ip, port) = vmpoolmgr.create_vm(lab_spec)
-            #(ip, port) = ("10.4.14.39", "8089")
+            lab_state = vmpoolmgr.create_vm(lab_spec)
+            ip = lab_state['vm_info']['vm_ip']
+            port = lab_state['vm_info']['vmm_port']
             vmmgrurl = "http://" + ip
-            update_state()
-            self.save_state()
-            if LabManager.test_lab(vmmgrurl, port, lab_src_url, version):
-                return ip
-            elif LabManager.test_lab(vmmgrurl, port, lab_src_url, version):
-                # retry seems to work (always?)
-                return ip
-            else:
-                Logging.LOGGER.error("Test failed")
+            try:
+                if LabManager.test_lab(vmmgrurl, port, lab_src_url, revision_tag):
+                    self.update_state(lab_state)
+                    return ip
+                elif LabManager.test_lab(vmmgrurl, port, lab_src_url, revision_tag):
+                    # retry seems to work (always?)
+                    self.update_state(lab_state)
+                    return ip
+                else:
+                    Logging.LOGGER.error("Test failed")
+            except Exception, e:
+                Logging.LOGGER.error("Test failed with error: " + str(e))
+            finally:
+                self.system.save()
         except Exception, e:
-            # This should return an error json when Controller is a web service
             Logging.LOGGER.error("Test failed with error: " + str(e))
+
+    def update_state(self, state):
+        state['lab_history']['released_by'] = 'dummy'
+        #state['lab_history']['released_on'] = strftime("%Y-%m-%d %H:%M:%S")
+        state['lab_history']['released_on'] = datetime.utcnow()
+
+    def undeploy_lab(self, lab_id):
+        Logging.LOGGER.debug("Controller.undeploy_lab for lab_id %s" % lab_id)
+        vmpoolmgr = VMPoolManager.VMPoolManager()
+        vmpoolmgr.undeploy_lab(lab_id)
+        return "Success"
 
 
 if __name__ == '__main__':
     c = Controller()
-    #c.test_lab("asdf", "asdf")
     #print c.test_lab("ovpl01", "https://github.com/nrchandan/vlab-computer-programming")
     print c.test_lab("ovpl01", "https://github.com/avinassh/cse09")
