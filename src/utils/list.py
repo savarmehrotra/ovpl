62df945f667d516e34eae3af16456872b7c14cf8
manasa <manasa.lingamallu@students.iiit.ac.in>
2014-06-12 16:39:38 +0530

    changes done to utils dir

---
+++ src/utils/list.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def list():
+        l=[]
+        p = subprocess.Popen('vzlist -a', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        for line in p.stdout.readlines():
+                print line
+                x=line.split()
+                l.append(x[0])
+
+list()
