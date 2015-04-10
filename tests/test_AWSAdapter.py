d7362cae3d6b90641fbf6a5b4418e0445cec3439
Ambika <ambika@vlabs.ac.in>
2015-04-10 18:09:18 +0530

    Tested both the cases, first one is a success while second is failing

--- tests/test_AWSAdapter.py
+++ tests/test_AWSAdapter.py
@@ -1,6 +1,6 @@
 import unittest
 import json
-import os
+import requests
 
 from src.adapters.AWSAdapter import AWSAdapter
 
@@ -15,12 +15,26 @@ class TestAWSAdapter(unittest.TestCase):
     def test_create_vm(self):
         vm_id = self.adapter.create_vm(self.lab_spec)
         vm_ip = self.adapter.get_vm_ip(vm_id)
+        instance = self.adapter.get_instance(vm_id)
+        print vm_ip
+        print vm_id
+        print instance
+        print instance.state_code
+        print "state code represents that instance is pending"
 
-        if os.system("ping -c 1 " + vm_ip) == 0:
-            print "VM successfully created"
+        self.assertIn(instance.state_code, (0, 16))
 
-        # TODO: testcases should have some assert statement. check python unit
-        # testing
+    def test_init_vm(self):
+     #   vm_id = self.adapter.create_vm(self.lab_spec)
+        lab_repo_name =  'ovpl'
+        result = self.adapter.init_vm(self.vm_id, lab_repo_name)
+        vm_ip = result[1]['vm_ip']
+        vmmanager_port = result[1]['vmm_port']
+    
+        response = requests.get("http://" + vm_ip + ":" + vmmanager_port)
+        self.assertEqual(response.status_code, 200)
+
+
 
 if __name__ == "__main__":
     unittest.main()
