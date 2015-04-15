1a0d20f91cf7d013b833a103df42cdead6ca3689
madhavipuliraju <madhavi@vlabs.ac.in>
2015-04-15 16:23:58 +0530

    Update test_openvz_adapter.py

--- tests/test_openvz_adapter.py
+++ tests/test_openvz_adapter.py
@@ -3,22 +3,21 @@ import json
 import requests
 import os
 
-from src.adapters.CentOSVZAdapter import CentOSVZAdapter
+from src.adapters.CentOSVZAdapter import CentOSVZAdapter, get_vm_ip
+vm_id = ''
 class TestCentOSVZAdapter(unittest.TestCase):
     def setUp(self):
         self.lab_spec = json.loads(open("scripts/labspec.json").read())
         self.adapter = CentOSVZAdapter()
 
     def test_create_vm(self):
-        vm_id = self.adapter.create_vm(self.lab_spec)
-        vm_ip = self.adapter.get_vm_ip(vm_id)
-        print vm_ip
+        global vm_id
+        (status, vm_id) = self.adapter.create_vm(self.lab_spec)
         print vm_id
-        hostname = "google.com"
-        response = os.system("ping -c 1 " + hostname)
-        self.assertTrue(response, 0)
+        vm_ip = get_vm_ip(vm_id)
+        response = os.system("ping -c 1 " + vm_ip)
+        self.assertEqual(response, 0)
 
 
 if __name__ == "__main__":
     unittest.main()
-
