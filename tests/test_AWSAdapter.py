5ac22f94215999f50576cb562b1f89e7f97b6452
Ambika <ambika@vlabs.ac.in>
2015-04-22 11:26:38 +0530

    Modified test case.
    - Separated the logic for calling =init_vm()= and for checking the values
      present in the result returned.
    
    - Now, =setup_test_vm_mgr_running()= calls =init_vm()= and returns the complete
      tuple as a result.
    
    - Test cases =test_init_vm()= and =test_vm_mgr_running()= separately calls the
      above setup and asserts if respective needs are met.

--- tests/test_AWSAdapter.py
+++ tests/test_AWSAdapter.py
@@ -5,8 +5,6 @@ import requests
 from src.adapters.AWSAdapter import AWSAdapter
 from src.LabManager import get_lab_reqs
 
-vm_ip = None
-vm_mgr_port = None
 
 class TestAWSAdapter(unittest.TestCase):
     def setUp(self):
@@ -18,35 +16,42 @@ class TestAWSAdapter(unittest.TestCase):
         get_lab_reqs("https://github.com/vlead/ovpl.git")
         result = self.adapter.create_vm(self.lab_spec)
         self.vm_id = result[1]
+#       print self.vm_id 
+
+    def setup_test_vm_mgr_running(self):
+#       print 'inside setup for test_vm_mgr_running'
+        result = self.adapter.init_vm(self.vm_id, self.repo_name)
+        return result
 
     def tearDown(self):
+#       print "tearing down" + self.vm_id
         self.adapter.destroy_vm(self.vm_id)        
 
     def test_create_vm(self):
         vm_ip = self.adapter.get_vm_ip(self.vm_id)
         instance = self.adapter.get_instance(self.vm_id)
        # print vm_ip
-       # print vm_id
-       # print instance
-       # print instance.state_code
-       # print "state code represents that instance is pending"
+       # print self.vm_id
 	# after an instance is created, it should be in one of the following states:
         # 0: instance creation pending, 16: instance running
         self.assertIn(instance.state_code, (0, 16))
 
     def test_init_vm(self):
-	global vm_ip
-        global vm_mgr_port
-        result = self.adapter.init_vm(vm_id, self.repo_name)
-       # print result
-        vm_ip = result[1]['vm_ip']
-        vm_mgr_port = result[1]['vmm_port']
-	self.assertTrue(result[0])
+#       print 'in init_vm'
+        result = self.setup_test_vm_mgr_running()
+#        print result
+        self.assertTrue(result[0])
 
     def test_vm_mgr_running(self):
-	vm_mgr_url = "http://{0}:{1}/api/1.0/test-lab".format(vm_ip, vm_mgr_port)
+#       print 'in third test case'
+        result = self.setup_test_vm_mgr_running()
+        vm_ip = result[1]['vm_ip']
+        vm_mgr_port = result[1]['vmm_port']
+#       print vm_ip
+#       print vm_mgr_port
+        vm_mgr_url = "http://{0}:{1}/api/1.0/test-lab".format(vm_ip, vm_mgr_port)
         response = requests.get(vm_mgr_url)
-       # print response
+#       print response
         self.assertEqual(response.status_code, 200)
 
 
