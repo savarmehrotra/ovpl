70c47989660afbe84a31d35e467c471c95f9f566
Anon Ray <rayanon004@gmail.com>
2015-04-22 18:12:05 +0530

    Merge two run test files
      - Merge the two run test files of CentOSVZAdapter and AWSAdapter

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,7 +2,12 @@
 # to start the http looger
 cd ../src/
 python http_logging/http_logging_server.py &
+
 # run the test cases
 cd ..
 python -m tests.test_CentOSVZAdapter
+python -m tests.test_AWSAdapter
 
+# stop the logging server
+cd src/
+make stop-server
