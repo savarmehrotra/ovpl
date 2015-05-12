814a218630afeda39a4138bc540667460168a8ba
khushpreet-kaur <khushpreet@vlabs.ac.in>
2015-05-12 10:03:52 +0530

    lint clean for files under /scripts

--- scripts/containers_list.py
+++ scripts/containers_list.py
@@ -1,17 +1,19 @@
 import sh
-CTID=[]
-containers_list=""
+CTID = []
+containers_list = ""
+
 
 def get_containers_list():
-    global CTID,containers_list
-    CTID=[]
-    containers_list=sh.vzlist("-a")
-    container=containers_list.split("\n")
-    container=container[1:]
+    global CTID, containers_list
+    CTID = []
+    containers_list = sh.vzlist("-a")
+    container = containers_list.split("\n")
+    container = container[1:]
     for index in range(len(container)-1):
-        p=container[index].split()
+        p = container[index].split()
         CTID.append(p[0])
     return CTID
+
 
 def print_containers_list():
     global containers_list
@@ -19,6 +21,5 @@ def print_containers_list():
 
 
 get_containers_list()
-if __name__ == '__main__' :
+if __name__ == '__main__':
     print_containers_list()
-
