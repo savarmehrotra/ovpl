039e5994a9a0420f6f3cdb11fdd9e5296c65b19e
Thirumal Ravula <travula@vlabs.ac.in>
2015-05-14 10:25:18 +0530

    save a record after deployment

--- src/State.py
+++ src/State.py
@@ -4,22 +4,12 @@ from src.singleton import Singleton
 
 @Singleton
 class State:
-    __state = []
+    db = None
 
     def __init__(self):
         self.db = pymongo.MongoClient().ovpl
-        self.restore()
 
-    def restore(self):
-        """Restores state from a mongodb collection."""
+    def save(self, record):
+        """Writes the information about the deployed lab to the database"""
         if "ovpl" in self.db.collection_names():
-            self.__state = list(self.db.ovpl.find())
-        else:
-            self.__state = []
-
-    def save(self):
-        """Writes the current state to mongodb(disk)"""
-        if "ovpl" in self.db.collection_names():
-            self.db.ovpl.rename("ovpl-last", dropTarget=True)
-        if bool(self.__state):
-            self.db.ovpl.insert(self.__state)
+            self.db.ovpl.insert(record)
