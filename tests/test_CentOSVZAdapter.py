ca5b9ca9ba1a5fd200fb87e0e4a578e091e37933
root <root@deploy.vlabs.ac.in>
2015-07-10 11:28:46 +0530

    invoke all changes according to new code conventions

--- tests/test_CentOSVZAdapter.py
+++ tests/test_CentOSVZAdapter.py
@@ -1,24 +1,27 @@
+from __init__ import *
 import unittest
 import requests
 import time
-from src.adapters.CentOSVZAdapter import CentOSVZAdapter
-from src.LabManager import get_lab_reqs
-from src.utils.execute_commands import execute_command
-from src.utils.git_commands import construct_repo_name
-from src.http_logging.http_logger import test_logger
-from src.LabManager import get_lab_reqs
+from adapters.centos_openvz_adapter import CentOSVZAdapter
+from lab_manager import LabManager
+from utils.execute_commands import execute_command
+from utils.git_commands import GitCommands
+from httplogging.http_logger import test_logger
+
 
 
 class TestCentOSVZAdapter(unittest.TestCase):
-    base_ip = "10.4.12.24"
+    base_ip = "10.2.56.230"
     lab_spec = ""
     vm_id = ""
     lab_repo_name = ""
 
     def setUp(self):
+        self.git = GitCommands()
+        self.lab = LabManager()
         lab_src_url = "https://github.com/Virtual-Labs/computer-programming-iiith.git"
-        self.lab_spec = get_lab_reqs(lab_src_url)
-        self.lab_repo_name = construct_repo_name(lab_src_url)
+        self.lab_spec = self.lab.get_lab_reqs(lab_src_url)
+        self.lab_repo_name = self.git.construct_repo_name(lab_src_url)
         self.adapter = CentOSVZAdapter()
         test_logger.debug("setUp(): lab_src_url = %s, lab_repo_name = %s" %
                           (lab_src_url, self.lab_repo_name))
@@ -53,8 +56,8 @@ class TestCentOSVZAdapter(unittest.TestCase):
         (status, self.vm_id) = self.adapter.create_vm(self.lab_spec)
         (status, result) = self.adapter.init_vm(self.vm_id, self.lab_repo_name)
         vm_ip = result['vm_ip']
-        vmm_port = result['vmm_port']
-        url = "http://%s:%s/api/1.0/test-lab" % (vm_ip, str(vmm_port))
+        vm_port = result['vm_port']
+        url = "http://%s:%s/api/1.0/test-lab" % (vm_ip, str(vm_port))
         test_logger.debug("VMMgr URL: %s" % url)
 
         for i in (1, 2, 4, 8, 16):
