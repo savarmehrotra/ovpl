929b62f788ec3fefd8eb270ed2de5c5968e18c90
ksripathi <kammari.sripathi@gmail.com>
2016-11-28 14:21:16 +0530

    added api code

--- src/google-oauth/api.py
+++ src/google-oauth/api.py
@@ -35,22 +35,32 @@ def index():
         lab_id = request.form.get("lab_id")
         lab_url = request.form.get("lab_src_url")
         tag = request.form.get("version")
+        if tag == "":
+            tag = "master"
         data = {'lab_id': lab_id, 'lab_src_url': lab_url, 'version': tag, 'key' : ADS_SECRET_KEY}
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-
-        if(re.search('^[a-zA-Z0-9_]+$', lab_id) is None):
+        
+        if(re.search('^[a-zA-Z0-9_-]+$', lab_id) is None):
             return render_template("index.html", message="Invalid Lab_id : " + lab_id)
-        
-        if (re.search('^[a-zA-Z0-9_]+$', tag) is None):
-            return render_template("index.html", message="Invalid Tag : " + tag)
-        
+        print tag
+        if (re.search('^[a-zA-Z0-9_-]+$', tag) is None or tag == ""):
+            if tag != "":
+                return render_template("index.html", message="Invalid Tag : " + tag)
+
+        if (re.search('(?P<host>(git@|https://)([\w\.@]+)(/|:))(?P<owner>[\w,\-,\_]+)/(?P<repo>[\w,\-,\_]+)(.git){0,1}((/){0,1})', lab_url) is None):
+            return render_template("index.html", message="Invalid Git URL : " + lab_url)
+
         r = requests.post(ADS_URL, data=json.dumps(data), headers=headers)
         if r.status_code == 200:
-            return render_template("index.html", message=r.text)
+            if r.text == "Test failed: See log file for errors":
+                return render_template("index.html", message="Something went wrong please check log files in ADS server")
+            else:
+                data['url'] = "http://" + r.text
+                return render_template("success.html", data=data)
         elif r.status_code == 401:
             return render_template("index.html", message="Unauthorized Credentials")
         else:
-            return render_template("index.html", message=r.status_code)
+            return render_template("index.html", message="Error : " + status_code)
             
 
 @api.route('/login')
