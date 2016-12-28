f1c0b909b8973577f8d59efd8420853b49b14856
ksripathi <kammari.sripathi@gmail.com>
2016-12-28 12:45:10 +0530

    added ads port 5000 to config.py

--- src/ads-web-app/config.py
+++ src/ads-web-app/config.py
@@ -2,7 +2,7 @@
 CONSUMER_KEY = "857206061842-11dem9m11firvs9164ig85q91vtnt8bd."\
                 "apps.googleusercontent.com"
 CONSUMER_SECRET = "WFEZLo8rTJ3uozckhIaNjQ5e"
-ADS_URL = "http://localhost:8080"
+ADS_URL = "http://localhost:5000"
 ADS_SECRET_KEY= "defaultkey"
 APP_URL = "http://ads.virtual-labs.ac.in:8080"
 # Configure your log paths
